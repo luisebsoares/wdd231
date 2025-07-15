173a8f3107e49c37af7936c5ebcad5dbed0f65f9
luisebsoarese <luisebsoares@hotmail.com>
2025-07-15 16:11:56 -0600

    week03

---
+++ scripts/weather.js
@@ -0,0 +1,37 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const myKey = "f10e1300b4738d2ee72db80ab5377b44"
+const myLat = "49.747054431062544";
+const myLon = "6.646609151890414";
+
+const url = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&appid=${myKey}&units=imperial`
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      displayResults(data); // uncomment when ready
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp.toFixed(1)}°F`;
+  captionDesc.textContent = data.weather[0].description;
+
+  const iconCode = data.weather[0].icon;
+  const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+  weatherIcon.setAttribute('src', iconUrl);
+  weatherIcon.setAttribute('alt', data.weather[0].description);
+}
+
+apiFetch();
